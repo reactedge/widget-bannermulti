672794c3f1808e7ac2285cf2df602852e04824d7
Herve Tribouilloy <herve@digitalrisedorset.co.uk>
2026-04-27 18:51:15 +0100

    Added global variable for the widget

--- vite_project/src/widget.ts
+++ vite_project/src/widget.ts
@@ -1,13 +1,13 @@
-import { mountWidget } from "./mountWidget";
+import {mountWidget, WIDGET_ID} from "./mountWidget";
 import './styles/banner.css'
 import type {BannerRawWidgetConfig} from "./BannerConfig.ts";
 
-export async function mount(el: HTMLElement, config: BannerRawWidgetConfig) {
-    if (!config) {
-        throw new Error('Config is required');
-    }
-
-    await mountWidget(el, config)
+const mount = async (el: HTMLElement, config: BannerRawWidgetConfig) => {
+     await mountWidget(el, config)
 }
 
+const api = { mount };
 
+(window as any)[`ReactEdge_${WIDGET_ID}`] = api;
+
+export { mount };
